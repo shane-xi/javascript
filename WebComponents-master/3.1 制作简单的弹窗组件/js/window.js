e1b5a5a9bda1185b19e3b8c7438d7f3f39749553
wozhishiyigeya <328399735@qq.com>
2016-08-07 22:06:23 +0800

    1

---
+++ WebComponents-master/3.1 制作简单的弹窗组件/js/window.js
@@ -0,0 +1,18 @@
+define(['jquery'],function($){
+	function Window(){}
+
+	Window.prototype = {
+		alert:function(content){
+			var boudingBox = $('<div class="window_boundingBox"></div>');
+			boudingBox.appendTo('body');
+			boudingBox.html(content);
+		},
+		confirm:function(){},
+		prompt:function(){}
+	}
+
+	/*return Window;*/
+	return {
+		Window: Window
+	}
+})
