9499aa4720f9fb9ceb809453064feb8035af4b90
shane-xi <328399735@qq.com>
2016-08-25 15:38:51 +0800

    1

---
+++ dom节点操作.js
@@ -0,0 +1,21 @@
+html5 classList API
+	<li class = "a b c"></li>li>
+element.classList; 返回类名  以数组形式["a","b","c"]
+增删改查
+element.classList.add('d','e');
+element.classList.remove('a','b');
+element.contains('')   返回布尔值
+element.toggle('a')   存在消掉，不存在补上
+
+节点的增删该查
+document.createDocumentFragment()
+append
+insertBefore
+getAttribute
+setAttribute
+removeAttribute
+hasChildnodes
+
+
+JQ
+清空节点  empty()
